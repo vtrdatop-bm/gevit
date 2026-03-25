dbadac62c8b3faaaac1216bbe39ad1c9d195a11e
ThiagoNery193 <engthiagonery@hotmail.com>
2026-03-25 14:05:18 -0500

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -46,6 +46,7 @@ export default function LoginPage() {
               className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring"
               placeholder="primeironome.sobrenome"
               autoFocus
+              style={{ textTransform: "none" }}
             />
           </div>
           <div className="space-y-2">
