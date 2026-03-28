7ff50f0391ce4f1990e4aaae2f00c020cac650ad
ThiagoNery193 <engthiagonery@hotmail.com>
2026-03-28 13:14:34 -0500

    Emergency fix: restoring mockData after cleanup regression

---
+++ src/mocks/mockData.ts
@@ -0,0 +1,4 @@
+import { ProcessoData, VistoriaData } from "@/types/database";
+
+export const KANBAN_MOCK_PROCESSOS: (ProcessoData & { vistoria: VistoriaData | null })[] = [];
+export const MAP_MOCK_PROCESSOS: (ProcessoData & { vistoria: VistoriaData | null })[] = [];
