87600925724ca18c99cd89f66f0c79f957fb0940
ThiagoNery193 <engthiagonery@hotmail.com>
2026-09-02 12:06:27 -0500

    Update MapPage.tsx

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -390,8 +390,8 @@ export default function MapPage() {
 
       L.control.zoom({ position: "bottomright" }).addTo(map);
 
-      L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/">OSM</a>',
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
       mapInstance.current = map;
